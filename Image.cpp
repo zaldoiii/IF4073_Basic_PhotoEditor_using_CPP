2fcc5cc514e4863861fc3f5ac54695bea5e3e25f
dandi99 <13517077@std.stei.itb.ac.id>
2021-02-17 16:06:00 +0700

    add hpp image

---
+++ Image.cpp
@@ -0,0 +1,28 @@
+#include "Image.hpp"
+
+Image::Image(int width, int height){
+    this->width = width;
+    this->height = height;
+}
+
+// Image :: ~Image(){
+
+// }
+
+int Image::getWidth()
+{
+	return this->width;
+}
+
+int Image::getHeight()
+{
+	return this->height;
+}
+
+void Image::setWidtht(int w){
+    this->width = w;
+}
+
+void Image::setHeight(int h){
+    this->height = h;
+}
---
+++ Image.hpp
@@ -0,0 +1,26 @@
+#ifndef IMAGE_HPP
+#define IMAGE_HPP
+
+class Image
+{
+    protected:
+        //atribute
+        int width;
+        int height;
+
+    public:
+        //constructor
+        Image(int width, int height);
+
+        //destroyer
+        ~Image();
+    
+        //method 
+        int getWidth();
+        int getHeight();
+        
+        void setWidth(int w);
+        void setHeight(int h);
+
+};
+#endif
