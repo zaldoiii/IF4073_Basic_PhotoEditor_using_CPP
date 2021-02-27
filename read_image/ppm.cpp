b1d9ef68cb9da427f3ababdfca54951ed2aed4b2
irfan harisw <harisw110@gmail.com>
2021-02-27 14:20:12 +0700

    update read ppm image

--- read_image/ppm.cpp
+++ read_image/ppm.cpp
@@ -4,96 +4,86 @@
 #include<iostream>
 using namespace std;
 
-typedef struct {
-     unsigned char red,green,blue;
-} PPMPixel;
-
-typedef struct {
-     int x, y;
-     PPMPixel *data;
-} PPMImage;
-
 #define RGB_COMPONENT_COLOR 255
 
-static PPMImage *readPPM(const char *filename)
+//read image from ppm file
+ColoredImg readPPM(const char *filename)
 {
-         char buff[16];
-         PPMImage *img;
-         FILE *fp;
-         int c, rgb_comp_color;
-         //open PPM file for reading
-         fp = fopen(filename, "rb");
-         if (!fp) {
-              cout<<"Unable to open file '%s'\n";
-              exit(1);
-         }
+     char buff[16];
+     FILE *fp;
+     int x, y, c, rgb_comp_color;
+     unsigned char *temp;
+     
+     //open PGM file for reading
+     fp = fopen(filename, "rb");
+     if (!fp) {
+          cout<<"Unable to open file '%s'\n";
+          exit(1);
+     }
 
-         //read image format
-         if (!fgets(buff, sizeof(buff), fp)) {
-              perror(filename);
-              exit(1);
-         }
+     //read image format
+     if (!fgets(buff, sizeof(buff), fp)) {
+          perror(filename);
+          exit(1);
+     }
 
-    //check the image format
-    if (buff[0] != 'P' || buff[1] != '6') {
-         cout<<"Invalid image format must be PPM\n";
-         exit(1);
-    }
+     //check the image format
+     if (buff[0] != 'P' || buff[1] != '6') {
+          cout<<"Invalid image format must be PPM\n";
+          exit(1);
+     }
 
-    //alloc memory form image
-    img = (PPMImage *)malloc(sizeof(PPMImage));
-    if (!img) {
-         cout<<"Unable to allocate memory\n";
-         exit(1);
-    }
+     //check for comments
+     c = getc(fp);
+     while (c == '#') {
+     while (getc(fp) != '\n') ;
+          c = getc(fp);
+     }
 
-    //check for comments
-    c = getc(fp);
-    while (c == '#') {
-    while (getc(fp) != '\n') ;
-         c = getc(fp);
-    }
+     ungetc(c, fp);
+     //read image size information
+     if (fscanf(fp, "%d %d", &x, &y) != 2) {
+          cout<<"Invalid image size (error loading '%s')\n";
+          exit(1);
+     }
 
-    ungetc(c, fp);
-    //read image size information
-    if (fscanf(fp, "%d %d", &img->x, &img->y) != 2) {
-         cout<<"Invalid image size (error loading '%s')\n";
-         exit(1);
-    }
+     //read rgb component
+     if (fscanf(fp, "%d", &rgb_comp_color) != 1) {
+          cout<<"Invalid rgb component (error loading '%s')\n";
+          exit(1);
+     }
 
-    //read rgb component
-    if (fscanf(fp, "%d", &rgb_comp_color) != 1) {
-         cout<<"Invalid rgb component (error loading '%s')\n";
-         exit(1);
-    }
+     //check rgb component depth
+     if (rgb_comp_color!= RGB_COMPONENT_COLOR) {
+          cout<<"'%s' does not have 8-bits components\n";
+          exit(1);
+     }
 
-    //check rgb component depth
-    if (rgb_comp_color!= RGB_COMPONENT_COLOR) {
-         cout<<"'%s' does not have 8-bits components\n";
-         exit(1);
-    }
+     while (fgetc(fp) != '\n') ;
+     //memory allocation for pixel data
+     temp = (unsigned char*)malloc(3* x * sizeof(unsigned char));
+     ColoredImg img(y, x, RGB_COMPONENT_COLOR);
 
-    while (fgetc(fp) != '\n') ;
-    //memory allocation for pixel data
-    img->data = (PPMPixel*)malloc(img->x * img->y * sizeof(PPMPixel));
+     if (!img.getMatriks()) {
+          cout<<"Unable to allocate memory\n";
+          exit(1);
+     }
 
-    if (!img) {
-         cout<<"Unable to allocate memory\n";
-         exit(1);
-    }
+     for (int i=0; i<img.getRows(); ++i)  {
+          fread(temp, sizeof(temp[0]), img.getCols()*3, fp);
+          for (int j = 0; j < img.getCols(); j++) {
+               for (int k = 0; k < 3; k++) {
+                    img.setCell(k,i,j,temp[j*3+k]);
+               }
+          } 
+     }
+     fclose(fp);
 
-    //read pixel data from file
-    if (fread(img->data, 3 * img->x, img->y, fp) != img->y) {
-         cout<<"Error loading image '%s'\n";
-         exit(1);
-    }
-
-    fclose(fp);
-    return img;
+     return img;
 }
 
-//Save File to PPM
-void writePPM(const char *filename, PPMImage *img)
+//save colored image to ppm file
+void writePPM(const char *filename, ColoredImg img)
 {
     FILE *fp;
     //open file for output
@@ -108,38 +98,30 @@ void writePPM(const char *filename, PPMImage *img)
     fprintf(fp, "P6\n");
 
     //image size
-    fprintf(fp, "%d %d\n",img->x,img->y);
-    cout<<"Size: "<<img->x<<"x"<<img->y<<"\n";
+    fprintf(fp, "%d %d\n", img.getCols(), img.getRows());
+    cout<<"Size: "<<img.getCols()<<"x"<<img.getRows()<<"\n";
 
     // rgb component depth
     fprintf(fp, "%d\n",RGB_COMPONENT_COLOR);
     cout<<RGB_COMPONENT_COLOR<<"\n";
 
+     unsigned char *temp;
+     temp = (unsigned char*)malloc(3* img.getCols() * sizeof(unsigned char));
+
     // pixel data
-    fwrite(img->data, 3 * img->x, img->y, fp);
-    
+     for (int i=0; i<img.getRows(); ++i) {
+          for (int j=0; j<img.getCols(); ++j)     {
+               for (int k=0; k<3; ++k)     {
+                    temp[j*3+k] = img.getCell(k,i,j);
+               }
+          }
+          fwrite(temp, sizeof(temp[0]), 3*img.getCols(), fp);
+     }
+     
     fclose(fp);
 }
 
-void changeColorPPM(PPMImage *img)
-{
-    int i;
-    if(img){
-
-         for(i=0;i<img->x*img->y;i++){
-              img->data[i].red=img->data[i].red;
-              img->data[i].green=img->data[i].green;
-              img->data[i].blue=img->data[i].blue;
-         }
-    }
-}
-
 int main(){
-    PPMImage *image;
-    image = readPPM("ppm_sample.ppm");
-    changeColorPPM(image);
-
+    ColoredImg image = readPPM("ppm_sample.ppm");
     writePPM("ppm_result.ppm",image);
-    cout<<"Press any key...";
-    getchar();
 }
